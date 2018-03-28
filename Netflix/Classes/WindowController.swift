97aa5b95f7c4ea700ea6ef8114aebf0a26ab8536
Daniel Farrelly <daniel@jellystyle.com>
2018-03-28 15:26:13 +1100

    Set a default and minimum window size

--- Netflix/Classes/WindowController.swift
+++ Netflix/Classes/WindowController.swift
@@ -5,10 +5,17 @@ class WindowController: NSWindowController {
 	override func windowDidLoad() {
 		super.windowDidLoad()
 
-		window?.title = ""
-		window?.styleMask.insert(.fullSizeContentView)
-		window?.titlebarAppearsTransparent = true
-		window?.isMovableByWindowBackground = true
+		guard let window = window else {
+			return
+		}
+
+		window.title = ""
+		window.styleMask.insert(.fullSizeContentView)
+		window.titlebarAppearsTransparent = true
+		window.isMovableByWindowBackground = true
+
+		window.setContentSize(NSSize(width: 1280, height: 904))
+		window.contentMinSize = NSSize(width: 640, height: 360)
 	}
 
 }
